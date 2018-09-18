09d05ebedfa9769c8a4d110d31309d55c52ed464
evdoo <o.evdokima@gmaol.com>
2018-09-18 14:55:29 +0300

    'apples and bread' added

---
+++ apples_and_bread/apples_and_bread.js
@@ -0,0 +1,7 @@
+let money = parseFloat(prompt("Сколько у вас с собой денег?"))
+let apples = parseInt(prompt("Сколько вы купили яблок?"))
+let bread = parseInt(prompt("Сколько вы купили батонов хлеба?"))
+let applesCost = parseFloat(prompt("Сколько стоит одно яблоко?")) * apples
+let breadCost = parseFloat(prompt("Сколько стоит один батон?")) * bread
+let compare = money >= (applesCost + breadCost)
+document.body.innerHtml(compare)
